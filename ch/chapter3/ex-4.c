5e66fd166bac3ab5aa05bc7f23dceaecbfdfa4b2
mana <ms00lx.221b@gmail.com>
2024-12-13 11:00:57 +0900

    第３章を終了

---
+++ ch/chapter3/ex-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int val = 3 + 5;
+    int val2 = 2 + 4;
+
+    printf("%d\n", val * val2);
+    
+    return 0;
+}
---
+++ ch/chapter3/ex-6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int val1 = (10 + 3 - 1) / 3;
+    int val2 = (10 + 4 - 1) / 4;
+    printf("%d\n", val1);
+    printf("%d\n", val2);
+    
+    return 0;
+}
