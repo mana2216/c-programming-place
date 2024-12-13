cba00ef386ac412e3f44f5e314136e8b05512aa3
mana <ms00lx.221b@gmail.com>
2024-12-13 13:12:59 +0900

    第５章を終了

---
+++ ch/chapter5/ex-2.c
@@ -0,0 +1,15 @@
+// エラーにならない
+#include <stdio.h>
+
+int main(void) {
+    /*
+    char input_sting[80];
+    /*
+    */
+    /*/
+    fgets(input_string, sizeof(input_string), stdin);
+    printf("%s", input_string);
+    */
+    
+    return 0;
+}
